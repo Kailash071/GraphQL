fe61e3fecdb1eac3515745ce89b4e0ebe923f584
Kailash Patel <patelkailash200@gmail.com>
2023-12-17 15:36:56 +0530

    schema typedefs and resolver of server graphql

---
+++ server/resolver.js
@@ -0,0 +1,16 @@
+import db from "./db.js"
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        authors() {
+            return db.authors
+        },
+        reviews() {
+            return db.reviews
+        }
+    }
+}
+export default resolvers;
