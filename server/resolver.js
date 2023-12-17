96a647a2f3269da903e7a640298da99711f086a1
Kailash Patel <patelkailash200@gmail.com>
2023-12-17 16:17:20 +0530

    schema and resolver of single game,author,review also same code of query's

--- server/resolver.js
+++ server/resolver.js
@@ -10,7 +10,53 @@ const resolvers = {
         },
         reviews() {
             return db.reviews
+        },
+        game(_,args) {
+            return db.games.find((game)=>game.id === args.id)
+        },
+        author(_,args) {
+            return db.authors.find((author)=>author.id === args.id)
+        },
+        review(_,args) {
+            return db.reviews.find((review)=>review.id === args.id)
         }
     }
 }
 export default resolvers;
+//############## client sample code of games,authors,reviews ############
+// query Authors {
+//     games {
+//       platform
+//       id
+//     }
+//     reviews {
+//       id
+//       rating
+//     }
+//     authors {
+//       verified
+//       name
+//       id
+//     }
+//   }
+//############## client sample code of game,author,review ############
+// query ExampleQuery($gameId: ID!, $reviewId: ID!, $authorId: ID!) {
+//     game(id: $gameId) {
+//       id,
+//       title
+//     }
+//     review(id: $reviewId) {
+//       id,
+//       content
+//     }
+//     author(id: $authorId) {
+//       id
+//       name
+//       verified
+//     }
+//   }  
+// {
+//     "gameId": 1,
+//     "reviewId": 2,
+//     "authorId": 3
+//   }
